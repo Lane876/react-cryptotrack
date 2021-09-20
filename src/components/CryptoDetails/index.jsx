25b395e6e7862c4a29e05588e8bdd40fcf3c8e56
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-09-20 23:00:36 +0200

    lineChart and cryptoDetails

--- src/components/CryptoDetails/index.jsx
+++ src/components/CryptoDetails/index.jsx
@@ -1,7 +1,183 @@
-import React from "react";
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import {
+  AiOutlineMoneyCollect,
+  AiOutlineDollarCircle,
+  AiOutlineFund,
+  AiOutlineExclamationCircle,
+  AiOutlineStop,
+  AiOutlineTrophy,
+  AiOutlineCheckCircle,
+  AiOutlineNumber,
+  AiOutlineThunderbolt,
+} from "react-icons/ai";
+
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../../services/cryptoApi";
+import LineChart from "../LineChart";
+import "./CryptoDetails.scss";
 
 const CryptoDetails = () => {
-  return <div>CryptoDetails</div>;
+  const { coinId } = useParams();
+  const [timeperiod, setTimeperiod] = useState("7d");
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({
+    coinId,
+    timeperiod,
+  });
+  const cryptoDetails = data?.data?.coin;
+
+  if (isFetching) return "Loading...";
+
+  const time = ["24h", "7d", "30d", "1y", "5y"];
+
+  const stats = [
+    {
+      title: "Price to USD",
+      value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
+      icon: <AiOutlineDollarCircle />,
+    },
+    { title: "Rank", value: cryptoDetails.rank, icon: <AiOutlineNumber /> },
+    {
+      title: "24h Volume",
+      value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+      icon: <AiOutlineThunderbolt />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
+      icon: <AiOutlineDollarCircle />,
+    },
+    {
+      title: "All-time-high(daily avg.)",
+      value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
+      icon: <AiOutlineTrophy />,
+    },
+  ];
+
+  const genericStats = [
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails.numberOfMarkets,
+      icon: <AiOutlineFund />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails.numberOfExchanges,
+      icon: <AiOutlineMoneyCollect />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails.approvedSupply ? (
+        <AiOutlineCheckCircle />
+      ) : (
+        <AiOutlineStop />
+      ),
+      icon: <AiOutlineExclamationCircle />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${millify(cryptoDetails.totalSupply)}`,
+      icon: <AiOutlineExclamationCircle />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
+      icon: <AiOutlineExclamationCircle />,
+    },
+  ];
+
+  return (
+    <div className="CoinDetail">
+      <div className="coin-heading-container">
+        <h4 className="coin-name">
+          {data?.data?.coin.name} ({data?.data?.coin.slug}) Price
+        </h4>
+        <p>
+          {cryptoDetails.name} live price in US Dollar (USD). View value
+          statistics, market cap and supply.
+        </p>
+      </div>
+      <select
+        defaultValue="7d"
+        className="select-timeperiod"
+        placeholder="Select Timeperiod"
+        onChange={(e) => setTimeperiod(e.target.value)}
+      >
+        {time.map((date) => (
+          <option key={date}>{date}</option>
+        ))}
+      </select>
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
+      <div className="stats-container">
+        <div className="coin-value-statistics">
+          <div className="coin-value-statistics-heading">
+            <h4 className="coin-details-heading">
+              {cryptoDetails.name} Value Statistics
+            </h4>
+            <p>
+              An overview showing the statistics of {cryptoDetails.name}, such
+              as the base and quote currency, the rank, and trading volume.
+            </p>
+          </div>
+          {stats.map(({ icon, title, value }) => (
+            <div className="coin-stats" key={value}>
+              <div className="coin-stats-name">
+                <p>{icon}</p>
+                <p>{title}</p>
+              </div>
+              <h4 className="stats">{value}</h4>
+            </div>
+          ))}
+        </div>
+        <div className="other-stats-info">
+          <div className="coin-value-statistics-heading">
+            <h4 className="coin-details-heading">Other Stats Info</h4>
+            <p>
+              An overview showing the statistics of {cryptoDetails.name}, such
+              as the base and quote currency, the rank, and trading volume.
+            </p>
+          </div>
+          {genericStats.map(({ icon, title, value }) => (
+            <div className="coin-stats">
+              <div className="coin-stats-name">
+                <p>{icon}</p>
+                <p>{title}</p>
+              </div>
+              <p className="stats">{value}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="coin-desc-link">
+        <div className="coin-desc">
+          <h4 className="coin-details-heading">
+            What is {cryptoDetails.name}?
+          </h4>
+          {HTMLReactParser(cryptoDetails.description)}
+        </div>
+        <div className="coin-links">
+          <h4 className="coin-details-heading">{cryptoDetails.name} Links</h4>
+          {cryptoDetails.links?.map((link) => (
+            <div className="coin-link" key={link.name}>
+              <h4 className="link-name">{link.type}</h4>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CryptoDetails;
---
+++ src/components/LineChart/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(
+      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
+    );
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      <div className="chart-header">
+        <h4 className="chart-title">{coinName} Price Chart </h4>
+        <div className="price-container">
+          <h4 className="price-change">Change: {coinHistory?.data?.change}%</h4>
+          <h4 className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </h4>
+        </div>
+      </div>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
