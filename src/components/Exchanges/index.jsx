9cd92b5cc74904ead51e049fb5ba8b7e1ec002aa
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-09-22 20:53:49 +0200

    exchange page

--- src/components/Exchanges/index.jsx
+++ src/components/Exchanges/index.jsx
@@ -1,7 +1,89 @@
 import React from "react";
+import millify from "millify";
+import HTMLReactParser from "html-react-parser";
+import { useState } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+
+import { useGetExchangesQuery } from "../../services/cryptoApi";
+import Loader from "../Loader";
+import "./Exchanges.scss";
 
 const Exchanges = () => {
-  return <div>Exchange</div>;
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+  const [isOpen, setIsopen] = useState(false);
+  const [index, setIndex] = useState(null);
+
+  if (isFetching) return <Loader />;
+
+  const handleClick = (i) => {
+    setIsopen(!isOpen);
+    setIndex(i);
+  };
+
+  const transitionVariant = {
+    hidden: {
+      y: -10,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      // transition: {
+      //   delay: 0.1,
+      //   type: "spring",
+      // },
+    },
+    exit: {
+      y: -20,
+      opacity: 0,
+    },
+  };
+
+  return (
+    <div className="Exchanges">
+      <div className="Exchanges__list">
+        {exchangesList.map((exchange, i) => (
+          <div>
+            <div
+              className="Exchanges__list--card"
+              key={i}
+              onClick={() => handleClick(i)}
+            >
+              <div className="Exchanges__list--card--header">
+                <strong style={{ marginRight: "1rem" }}>
+                  {exchange.rank}.
+                </strong>
+                <img
+                  className="exchange-image"
+                  src={exchange.iconUrl}
+                  style={{ width: "30px", height: "30px", marginRight: "1rem" }}
+                />
+                <strong>{exchange.name}</strong>
+              </div>
+
+              <div>24h Trade Volume: ${millify(exchange.volume)}</div>
+              <div>Markets: {millify(exchange.numberOfMarkets)}</div>
+              <div>Change: {millify(exchange.marketShare)}%</div>
+            </div>
+            <AnimatePresence>
+              {isOpen && index === i && (
+                <motion.div
+                  variants={transitionVariant}
+                  animate="visible"
+                  initial="hidden"
+                  exit="exit"
+                  className="description"
+                >
+                  {HTMLReactParser(exchange.description || "")}
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Exchanges;
