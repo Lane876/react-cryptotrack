707dbc5bb9404876f7a6556866f999bf3bbdb86e
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-09-23 22:05:30 +0200

    sidebar & backdrop

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Rotate } from "hamburger-react";
+import Backdrop from "../Backdrop";
+import { AnimatePresence, motion } from "framer-motion";
+import { HiMenuAlt1, HiX } from "react-icons/hi";
 
-import "./SideMenu.css";
+import "./SideMenu.scss";
 
 const SideMenu = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -27,20 +29,42 @@ const SideMenu = () => {
   }, [sidebar]);
   return (
     <>
-      <Rotate color="white" toggled={sidebar} toggle={setSidebar} />
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <div className="nav-menu-items">
-          {sidebarData.map((item, index) => {
-            return (
-              <div key={index} className={item.cName}>
-                <Link to={item.path} onClick={showSidebar}>
-                  <span>{item.title}</span>
-                </Link>
+      <HiMenuAlt1 onClick={showSidebar} color="white" size="30" />
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+      >
+        {sidebar && (
+          <Backdrop onClick={() => setSidebar(false)}>
+            <motion.nav
+              className="Nav"
+              onClick={(e) => e.stopPropagation()}
+              initial={{ x: "200%" }}
+              animate={{ x: "0", transition: { stiffness: 10 } }}
+              exit={{ x: "200%" }}
+            >
+              <HiX
+                className="Nav__close"
+                onClick={showSidebar}
+                color="white"
+                size="30"
+              />
+              <div className="Nav__items">
+                {sidebarData.map((item, index) => {
+                  return (
+                    <div key={index} className={item.cName}>
+                      <Link to={item.path} onClick={showSidebar}>
+                        <span>{item.title}</span>
+                      </Link>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-      </nav>
+            </motion.nav>
+          </Backdrop>
+        )}
+      </AnimatePresence>
     </>
   );
 };
