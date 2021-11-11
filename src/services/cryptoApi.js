6c4b82079edc7178051ae5d3f7b87d21fe89a174
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-11-11 16:13:28 +0100

    x-access-token=i-have-to-migrate-to-v2

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
   "x-rapidapi-key": "5c4f04d078msh1d8dd81817290a6p12f9f4jsn60b3f37ef8dc",
+  "x-access-token": "i-have-to-migrate-to-v2",
 };
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
